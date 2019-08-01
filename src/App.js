da3c9f906847f39bdafe8abe3b47fb3a58c4dd3c
jondauz <jondauz@gmail.com>
2019-07-31 19:33:46 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.scss';
+import TriviaApp from './containers/TriviaApp';
+
+function App() {
+  return (
+    <TriviaApp />
+  );
+}
+
+export default App;
---
+++ src/containers/TriviaApp/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class TriviaApp extends React.Component {
+
+    componentDidMount() {
+        console.log('asdf');
+    }
+
+    render() {
+        return(
+            <p>Bitch ass mother</p>
+        )
+    }
+}
+
+export default TriviaApp;
