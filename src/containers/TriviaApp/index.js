56fa517dfcc1ec50620a6b899467d3c3d15d5b03
Jonathan Dauz <jondauz@gmail.com>
2019-08-01 08:13:06 -0700

    fetch questions

--- src/containers/TriviaApp/index.js
+++ src/containers/TriviaApp/index.js
@@ -1,15 +1,108 @@
 import React from 'react';
 
 class TriviaApp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          error: null,
+          isLoaded: false,
+          questions: []
+        };
+      }
+    
 
     componentDidMount() {
-        console.log('asdf');
+        let apiRoot = "https://opentdb.com",
+            numOfCategories = 5,
+            chosenCategories = [],
+            amountOfEasyQuestions = 2,
+            amountOfMediumQuestions = 2,
+            amountOfHardQuestions = 1;
+            
+        fetch(`${apiRoot}/api_category.php`)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    let categories = result.trivia_categories;
+                    categories.sort(() => Math.random() - 0.5);
+
+                    for(let i = 1; i <= numOfCategories; i++) {
+                        chosenCategories.push(categories[i]);
+                    }
+
+                    console.log(chosenCategories);
+                    return Promise.all(chosenCategories.map(category => fetch(`${apiRoot}/api.php?amount=${amountOfEasyQuestions}&category=${category.id}&difficulty=easy`).then(res => res.json())));
+                },
+                this.errorHandler
+            )
+            .then(
+                (result) => {
+                    console.log('EASY');
+                    console.log(result);
+
+                    chosenCategories.forEach((category, index) => {
+                        category.questions = [...result[index].results]
+                    });
+
+                    return Promise.all(chosenCategories.map(category => fetch(`${apiRoot}/api.php?amount=${amountOfMediumQuestions}&category=${category.id}&difficulty=medium`).then(res => res.json())));
+                },
+                this.errorHandler
+            )
+            .then(
+                (result) => {
+                    console.log('MEDIUM');
+                    console.log(result);
+
+                    chosenCategories.forEach((category, index) => {
+                        category.questions = [...category.questions, ...result[index].results]
+                    });
+
+                    return Promise.all(chosenCategories.map(category => fetch(`${apiRoot}/api.php?amount=${amountOfHardQuestions}&category=${category.id}&difficulty=hard`).then(res => res.json())));
+                },
+                this.errorHandler
+            )
+            .then(
+                (result) => {
+                    console.log('HARD');
+                    console.log(result);
+
+                    chosenCategories.forEach((category, index) => {
+                        category.questions = [...category.questions, ...result[index].results]
+                    });
+
+                    this.setState({
+                        isLoaded: true,
+                        questions: chosenCategories
+                    });
+                }
+            );
+            
+    }
+
+    errorHandler(error) {
+        this.setState({
+            isLoaded: true,
+            error
+        });
     }
 
     render() {
-        return(
-            <p>Bitch ass mother</p>
-        )
+        const { error, isLoaded, questions } = this.state;
+        if (error) {
+            return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div>Loading...</div>;
+        } else {
+            return (
+                <ul>
+                    {questions.map(question => (
+                        <li key={question.id}>
+                        {question.name}
+                        </li>
+                    ))}
+                </ul>
+            );
+        }
     }
 }
 
