00c08bc87a8bf96371a7c94a860282ab83747023
Jonathan Dauz <jondauz@gmail.com>
2019-08-01 22:15:26 -0700

    add continue button

--- src/components/AnswerFeedbackComponent/AnswerFeedbackComponent.js
+++ src/components/AnswerFeedbackComponent/AnswerFeedbackComponent.js
@@ -20,12 +20,13 @@ class AnswerFeedbackComponent extends React.Component {
     render() {
         const { isCorrect, scoreChange, prevScore, correctAnswer } = this.props.result;
         return (
-            <div className="feedback__container" onClick={ this.handleCloseSelf }>
+            <div className="feedback__container">
                 <div className="feedback__content">
                     <p>{ isCorrect ? "Correct!" : "Wrong!" } The answer is "{ this.decodeText(correctAnswer) }"</p>
                     <p>{ scoreChange }</p>
                     <p>Previous Score: { prevScore }</p>
                     <p>New Score: { this.props.score }</p>
+                    <button onClick={ this.handleCloseSelf } type="button">Continue</button>
                 </div>
             </div>
         )
